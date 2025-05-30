61d2ed964599a70498d744452fd26d1394242b3e
Amans-2201 <160013204+Amans-2201@users.noreply.github.com>
2025-05-30 23:42:08 +0530

    added print statements to make message user friendly

--- scripts/crm_bulk_insert.sql
+++ scripts/crm_bulk_insert.sql
@@ -11,8 +11,16 @@ CREATE OR ALTER PROCEDURE bronze.load_crm_bronze_data
 AS
 BEGIN
     IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    BEGIN
+        PRINT '================================================================';
+        PRINT '>>Truncating table bronze.crm_cust_info';
         TRUNCATE TABLE bronze.crm_cust_info;
+    END
+    ELSE
+        PRINT '>>Table bronze.crm_cust_info does not exist.';
+
     -- Bulk Insert CRM Customer Information
+    PRINT '>>Bulk inserting data into bronze.crm_cust_info ';
     Bulk Insert bronze.crm_cust_info
     -- Save the .csv dataset files in the specified path or modify the path accordingly
     FROM 'D:\Github\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
@@ -22,12 +30,20 @@ BEGIN
         TABLOCK,                -- Use table-level locking for the bulk insert
         ROWTERMINATOR = '\n',   -- Specify the row terminator
         FORMAT = 'CSV'          -- Specify the format as CSV
-    );  
-
+    );
+    PRINT '>>Bulk insert into bronze.crm_cust_info completed.';
+    PRINT '================================================================';
     -- Truncate the table bronze.crm_prd_info
     IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    BEGIN
+        PRINT '>>Truncating table bronze.crm_prd_info';
         TRUNCATE TABLE bronze.crm_prd_info;
+    END
+    ELSE
+        PRINT '>>Table bronze.crm_prd_info does not exist.';
+    
     -- Bulk Insert CRM Product Information
+    PRINT '>>Bulk inserting data into bronze.crm_prd_info ';
     Bulk Insert bronze.crm_prd_info
     FROM 'D:\Github\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'    
     WITH (
@@ -37,12 +53,21 @@ BEGIN
         ROWTERMINATOR = '\n',   -- Specify the row terminator
         FORMAT = 'CSV'          -- Specify the format as CSV
     );
+    PRINT '>>Bulk insert into bronze.crm_prd_info completed.';
+    PRINT '================================================================';
 
     -- Truncate the table bronze.crm_sales_details
     IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
-        TRUNCATE TABLE bronze.crm_sales_details;    
+    BEGIN
+        PRINT '>>Truncating table bronze.crm_sales_details';
+        TRUNCATE TABLE bronze.crm_sales_details;
+    END
+    ELSE
+        PRINT '>>Table bronze.crm_sales_details does not exist.';
+
     -- Bulk Insert CRM Sales Details
-    Bulk Insert bronze.crm_sales_details    
+    PRINT '>>Bulk inserting data into bronze.crm_sales_details ';
+    Bulk Insert bronze.crm_sales_details
     FROM 'D:\Github\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
     WITH (
         FIRSTROW = 2,           -- Skip the header row
@@ -51,7 +76,11 @@ BEGIN
         ROWTERMINATOR = '\n',   -- Specify the row terminator
         FORMAT = 'CSV'          -- Specify the format as CSV
     );
-END  
+    PRINT '>>Bulk insert into bronze.crm_sales_details completed.';
+    PRINT '================================================================';
+    PRINT '>>All CRM data has been successfully loaded into the bronze layer.';
+    PRINT '================================================================';
+END
 -- Execute the procedure to load data into the bronze layer CRM
 GO
 EXECUTE bronze.load_crm_bronze_data;
--- scripts/erp_bulk_insert.sql
+++ scripts/erp_bulk_insert.sql
@@ -1,7 +1,6 @@
 Use DataWarehouse;
 GO
 /*
-/*
 ============================================================
 This script inserts data into the bronze layer for ERP Tables
 Create or alter procedure bronze.load_erp_bronze_data
@@ -12,8 +11,16 @@ AS
 BEGIN
     -- Truncate the tables if they already exist to ensure fresh data insertion
     IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    BEGIN
+        PRINT '================================================================';
+        PRINT 'Truncating table bronze.erp_cust_az12';
         TRUNCATE TABLE bronze.erp_cust_az12;
-    -- Bulk Insert ERP Customer AZ12    
+    END
+    ELSE
+        PRINT 'Table bronze.erp_cust_az12 does not exist.';
+    
+    -- Bulk Insert ERP Customer AZ12 
+    PRINT 'Bulk inserting data into bronze.erp_cust_az12 ';   
     Bulk Insert bronze.erp_cust_az12
     -- Save the .csv dataset files in the specified path or modify the path accordingly
     FROM 'D:\Github\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
@@ -24,11 +31,20 @@ BEGIN
         ROWTERMINATOR = '\n',   -- Specify the row terminator
         FORMAT = 'CSV'          -- Specify the format as CSV
     );
+    PRINT 'Bulk insert into bronze.erp_cust_az12 completed.';
+    PRINT '================================================================';
     
     -- Truncate the table bronze.erp_loc_a101
     IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
-        TRUNCATE TABLE bronze.erp_loc_a101; 
-    -- Bulk Insert ERP Location A101    
+    BEGIN
+        PRINT 'Truncating table bronze.erp_loc_a101';
+        TRUNCATE TABLE bronze.erp_loc_a101;
+    END
+    ELSE
+        PRINT 'Table bronze.erp_loc_a101 does not exist.';
+
+    -- Bulk Insert ERP Location A101  
+    PRINT 'Bulk inserting data into bronze.erp_loc_a101 ';  
     Bulk Insert bronze.erp_loc_a101
     FROM 'D:\Github\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
     WITH (
@@ -38,11 +54,20 @@ BEGIN
         ROWTERMINATOR = '\n',   -- Specify the row terminator
         FORMAT = 'CSV'          -- Specify the format as CSV
     );
-    
+    PRINT 'Bulk insert into bronze.erp_loc_a101 completed.';
+    PRINT '================================================================';
+
     -- Truncate the table bronze.erp_px_cat_g1v2
     IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    BEGIN
+        PRINT 'Truncating table bronze.erp_px_cat_g1v2';
         TRUNCATE TABLE bronze.erp_px_cat_g1v2; 
+    END
+    ELSE
+        PRINT 'Table bronze.erp_px_cat_g1v2 does not exist.';
+    
     -- Bulk Insert ERP Product Category G1V2
+    PRINT 'Bulk inserting data into bronze.erp_px_cat_g1v2 ';
     Bulk Insert bronze.erp_px_cat_g1v2  
     FROM 'D:\Github\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
     WITH (
@@ -52,6 +77,10 @@ BEGIN
         ROWTERMINATOR = '\n',   -- Specify the row terminator
         FORMAT = 'CSV'          -- Specify the format as CSV
     );  
+    PRINT 'Bulk insert into bronze.erp_px_cat_g1v2 completed.';
+    PRINT '================================================================';
+    PRINT '>>All ERP data has been successfully loaded into the bronze layer.';
+    PRINT '================================================================';
 END
 -- execute the procedure to load data into the bronze layer ERP
 GO
