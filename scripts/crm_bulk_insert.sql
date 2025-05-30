1dcb2aacf7262519fe73c910ec721d348ef2db31
Amans-2201 <160013204+Amans-2201@users.noreply.github.com>
2025-05-30 23:05:58 +0530

    convert insert scripts to stored procedure

--- scripts/crm_bulk_insert.sql
+++ scripts/crm_bulk_insert.sql
@@ -1,55 +1,61 @@
 Use DataWarehouse;
 GO
--- This script inserts data into the bronze layer for CRM customer information
+/*
+============================================================
+This script inserts data into the bronze layer for ERP Tables
+Create or alter procedure bronze.load_crm_bronze_data
+============================================================
+*/
 
--- Truncate the tables if they already exist to ensure fresh data insertion
-IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
-    TRUNCATE TABLE bronze.crm_cust_info;
-GO
--- Bulk Insert CRM Customer Information
-Bulk Insert bronze.crm_cust_info
--- Save the .csv dataset files in the specified path or modify the path accordingly
-FROM 'D:\Github\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-WITH (
-    FIRSTROW = 2,           -- Skip the header row
-    FIELDTERMINATOR = ',',  -- Specify the field terminator
-    TABLOCK,                -- Use table-level locking for the bulk insert
-    ROWTERMINATOR = '\n',   -- Specify the row terminator
-    FORMAT = 'CSV'          -- Specify the format as CSV
-);  
-GO
+CREATE OR ALTER PROCEDURE bronze.load_crm_bronze_data   
+AS
+BEGIN
+    IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+        TRUNCATE TABLE bronze.crm_cust_info;
+    -- Bulk Insert CRM Customer Information
+    Bulk Insert bronze.crm_cust_info
+    -- Save the .csv dataset files in the specified path or modify the path accordingly
+    FROM 'D:\Github\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+    WITH (
+        FIRSTROW = 2,           -- Skip the header row
+        FIELDTERMINATOR = ',',  -- Specify the field terminator
+        TABLOCK,                -- Use table-level locking for the bulk insert
+        ROWTERMINATOR = '\n',   -- Specify the row terminator
+        FORMAT = 'CSV'          -- Specify the format as CSV
+    );  
 
--- Truncate the table bronze.crm_prd_info
-IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
-    TRUNCATE TABLE bronze.crm_prd_info;
-GO
--- Bulk Insert CRM Product Information
-Bulk Insert bronze.crm_prd_info
-FROM 'D:\Github\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'    
-WITH (
-    FIRSTROW = 2,           -- Skip the header row
-    FIELDTERMINATOR = ',',  -- Specify the field terminator
-    TABLOCK,                -- Use table-level locking for the bulk insert
-    ROWTERMINATOR = '\n',   -- Specify the row terminator
-    FORMAT = 'CSV'          -- Specify the format as CSV
-);
-GO
+    -- Truncate the table bronze.crm_prd_info
+    IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+        TRUNCATE TABLE bronze.crm_prd_info;
+    -- Bulk Insert CRM Product Information
+    Bulk Insert bronze.crm_prd_info
+    FROM 'D:\Github\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'    
+    WITH (
+        FIRSTROW = 2,           -- Skip the header row
+        FIELDTERMINATOR = ',',  -- Specify the field terminator
+        TABLOCK,                -- Use table-level locking for the bulk insert
+        ROWTERMINATOR = '\n',   -- Specify the row terminator
+        FORMAT = 'CSV'          -- Specify the format as CSV
+    );
 
--- Truncate the table bronze.crm_sales_details
-IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
-    TRUNCATE TABLE bronze.crm_sales_details;    
+    -- Truncate the table bronze.crm_sales_details
+    IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+        TRUNCATE TABLE bronze.crm_sales_details;    
+    -- Bulk Insert CRM Sales Details
+    Bulk Insert bronze.crm_sales_details    
+    FROM 'D:\Github\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+    WITH (
+        FIRSTROW = 2,           -- Skip the header row
+        FIELDTERMINATOR = ',',  -- Specify the field terminator
+        TABLOCK,                -- Use table-level locking for the bulk insert
+        ROWTERMINATOR = '\n',   -- Specify the row terminator
+        FORMAT = 'CSV'          -- Specify the format as CSV
+    );
+END  
+-- Execute the procedure to load data into the bronze layer CRM
 GO
--- Bulk Insert CRM Sales Details
-Bulk Insert bronze.crm_sales_details    
-FROM 'D:\Github\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-WITH (
-    FIRSTROW = 2,           -- Skip the header row
-    FIELDTERMINATOR = ',',  -- Specify the field terminator
-    TABLOCK,                -- Use table-level locking for the bulk insert
-    ROWTERMINATOR = '\n',   -- Specify the row terminator
-    FORMAT = 'CSV'          -- Specify the format as CSV
-);  
-
+EXECUTE bronze.load_crm_bronze_data;
+GO
 --Verify the data has been inserted correctly
 GO
 SELECT COUNT(*) FROM bronze.crm_cust_info;
--- scripts/erp_bulk_insert.sql
+++ scripts/erp_bulk_insert.sql
@@ -1,54 +1,61 @@
 Use DataWarehouse;
 GO
--- This script inserts data into the bronze layer for ERP Tables
-
--- Truncate the tables if they already exist to ensure fresh data insertion
-IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
-    TRUNCATE TABLE bronze.erp_cust_az12;
+/*
+/*
+============================================================
+This script inserts data into the bronze layer for ERP Tables
+Create or alter procedure bronze.load_erp_bronze_data
+============================================================
+*/
+Create or alter procedure bronze.load_erp_bronze_data
+AS
+BEGIN
+    -- Truncate the tables if they already exist to ensure fresh data insertion
+    IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+        TRUNCATE TABLE bronze.erp_cust_az12;
+    -- Bulk Insert ERP Customer AZ12    
+    Bulk Insert bronze.erp_cust_az12
+    -- Save the .csv dataset files in the specified path or modify the path accordingly
+    FROM 'D:\Github\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+    WITH (
+        FIRSTROW = 2,           -- Skip the header row
+        FIELDTERMINATOR = ',',  -- Specify the field terminator
+        TABLOCK,                -- Use table-level locking for the bulk insert
+        ROWTERMINATOR = '\n',   -- Specify the row terminator
+        FORMAT = 'CSV'          -- Specify the format as CSV
+    );
+    
+    -- Truncate the table bronze.erp_loc_a101
+    IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+        TRUNCATE TABLE bronze.erp_loc_a101; 
+    -- Bulk Insert ERP Location A101    
+    Bulk Insert bronze.erp_loc_a101
+    FROM 'D:\Github\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+    WITH (
+        FIRSTROW = 2,           -- Skip the header row
+        FIELDTERMINATOR = ',',  -- Specify the field terminator
+        TABLOCK,                -- Use table-level locking for the bulk insert
+        ROWTERMINATOR = '\n',   -- Specify the row terminator
+        FORMAT = 'CSV'          -- Specify the format as CSV
+    );
+    
+    -- Truncate the table bronze.erp_px_cat_g1v2
+    IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+        TRUNCATE TABLE bronze.erp_px_cat_g1v2; 
+    -- Bulk Insert ERP Product Category G1V2
+    Bulk Insert bronze.erp_px_cat_g1v2  
+    FROM 'D:\Github\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+    WITH (
+        FIRSTROW = 2,           -- Skip the header row
+        FIELDTERMINATOR = ',',  -- Specify the field terminator
+        TABLOCK,                -- Use table-level locking for the bulk insert
+        ROWTERMINATOR = '\n',   -- Specify the row terminator
+        FORMAT = 'CSV'          -- Specify the format as CSV
+    );  
+END
+-- execute the procedure to load data into the bronze layer ERP
 GO
--- Bulk Insert ERP Customer AZ12    
-Bulk Insert bronze.erp_cust_az12
--- Save the .csv dataset files in the specified path or modify the path accordingly
-FROM 'D:\Github\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
-WITH (
-    FIRSTROW = 2,           -- Skip the header row
-    FIELDTERMINATOR = ',',  -- Specify the field terminator
-    TABLOCK,                -- Use table-level locking for the bulk insert
-    ROWTERMINATOR = '\n',   -- Specify the row terminator
-    FORMAT = 'CSV'          -- Specify the format as CSV
-);
-GO
-
--- Truncate the table bronze.erp_loc_a101
-IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
-    TRUNCATE TABLE bronze.erp_loc_a101; 
-GO
--- Bulk Insert ERP Location A101    
-Bulk Insert bronze.erp_loc_a101
-FROM 'D:\Github\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
-WITH (
-    FIRSTROW = 2,           -- Skip the header row
-    FIELDTERMINATOR = ',',  -- Specify the field terminator
-    TABLOCK,                -- Use table-level locking for the bulk insert
-    ROWTERMINATOR = '\n',   -- Specify the row terminator
-    FORMAT = 'CSV'          -- Specify the format as CSV
-);
-GO
-
--- Truncate the table bronze.erp_px_cat_g1v2
-IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
-    TRUNCATE TABLE bronze.erp_px_cat_g1v2; 
-GO  
--- Bulk Insert ERP Product Category G1V2
-Bulk Insert bronze.erp_px_cat_g1v2  
-FROM 'D:\Github\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
-WITH (
-    FIRSTROW = 2,           -- Skip the header row
-    FIELDTERMINATOR = ',',  -- Specify the field terminator
-    TABLOCK,                -- Use table-level locking for the bulk insert
-    ROWTERMINATOR = '\n',   -- Specify the row terminator
-    FORMAT = 'CSV'          -- Specify the format as CSV
-);  
+EXECUTE bronze.load_erp_bronze_data;
 GO
 -- Verify the data has been inserted correctly  
 SELECT COUNT(*) FROM bronze.erp_cust_az12;
