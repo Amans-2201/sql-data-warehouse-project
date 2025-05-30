7c728a9dc95090d35a7cee5204677d1c09683b7c
Amans-2201 <160013204+Amans-2201@users.noreply.github.com>
2025-05-31 00:06:09 +0530

    Error handling and duration to load records

--- scripts/crm_bulk_insert.sql
+++ scripts/crm_bulk_insert.sql
@@ -10,76 +10,100 @@ Create or alter procedure bronze.load_crm_bronze_data
 CREATE OR ALTER PROCEDURE bronze.load_crm_bronze_data   
 AS
 BEGIN
-    IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
-    BEGIN
+    DECLARE @startTime DATETIME, @endTime DATETIME;
+    BEGIN TRY
+        IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+        BEGIN
+            PRINT '================================================================';
+            PRINT '>>Truncating table bronze.crm_cust_info';
+            TRUNCATE TABLE bronze.crm_cust_info;
+        END
+        ELSE
+            PRINT '>>Table bronze.crm_cust_info does not exist.';
+
+        SET @startTime = GETDATE();  -- Record the start time of the operation
+        -- Bulk Insert CRM Customer Information
+        PRINT '>>Bulk inserting data into bronze.crm_cust_info ';
+        Bulk Insert bronze.crm_cust_info
+        -- Save the .csv dataset files in the specified path or modify the path accordingly
+        FROM 'D:\Github\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+        WITH (
+            FIRSTROW = 2,           -- Skip the header row
+            FIELDTERMINATOR = ',',  -- Specify the field terminator
+            TABLOCK,                -- Use table-level locking for the bulk insert
+            ROWTERMINATOR = '\n',   -- Specify the row terminator
+            FORMAT = 'CSV'          -- Specify the format as CSV
+        );
+        SET @endTime = GETDATE();  -- Record the end time of the operation
+        PRINT '>> LOAD DURATION: ' + CAST(DATEDIFF(SECOND, @startTime, @endTime) AS NVARCHAR(10)) + ' seconds';
+        PRINT '>>Bulk insert into bronze.crm_cust_info completed.';
         PRINT '================================================================';
-        PRINT '>>Truncating table bronze.crm_cust_info';
-        TRUNCATE TABLE bronze.crm_cust_info;
-    END
-    ELSE
-        PRINT '>>Table bronze.crm_cust_info does not exist.';
+        -- Truncate the table bronze.crm_prd_info
+        IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+        BEGIN
+            PRINT '>>Truncating table bronze.crm_prd_info';
+            TRUNCATE TABLE bronze.crm_prd_info;
+        END
+        ELSE
+            PRINT '>>Table bronze.crm_prd_info does not exist.';
 
-    -- Bulk Insert CRM Customer Information
-    PRINT '>>Bulk inserting data into bronze.crm_cust_info ';
-    Bulk Insert bronze.crm_cust_info
-    -- Save the .csv dataset files in the specified path or modify the path accordingly
-    FROM 'D:\Github\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-    WITH (
-        FIRSTROW = 2,           -- Skip the header row
-        FIELDTERMINATOR = ',',  -- Specify the field terminator
-        TABLOCK,                -- Use table-level locking for the bulk insert
-        ROWTERMINATOR = '\n',   -- Specify the row terminator
-        FORMAT = 'CSV'          -- Specify the format as CSV
-    );
-    PRINT '>>Bulk insert into bronze.crm_cust_info completed.';
-    PRINT '================================================================';
-    -- Truncate the table bronze.crm_prd_info
-    IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
-    BEGIN
-        PRINT '>>Truncating table bronze.crm_prd_info';
-        TRUNCATE TABLE bronze.crm_prd_info;
-    END
-    ELSE
-        PRINT '>>Table bronze.crm_prd_info does not exist.';
-    
-    -- Bulk Insert CRM Product Information
-    PRINT '>>Bulk inserting data into bronze.crm_prd_info ';
-    Bulk Insert bronze.crm_prd_info
-    FROM 'D:\Github\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'    
-    WITH (
-        FIRSTROW = 2,           -- Skip the header row
-        FIELDTERMINATOR = ',',  -- Specify the field terminator
-        TABLOCK,                -- Use table-level locking for the bulk insert
-        ROWTERMINATOR = '\n',   -- Specify the row terminator
-        FORMAT = 'CSV'          -- Specify the format as CSV
-    );
-    PRINT '>>Bulk insert into bronze.crm_prd_info completed.';
-    PRINT '================================================================';
+        SET @startTime = GETDATE();  -- Record the start time of the operation
+        -- Bulk Insert CRM Product Information
+        PRINT '>>Bulk inserting data into bronze.crm_prd_info ';
+        Bulk Insert bronze.crm_prd_info
+        FROM 'D:\Github\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'    
+        WITH (
+            FIRSTROW = 2,           -- Skip the header row
+            FIELDTERMINATOR = ',',  -- Specify the field terminator
+            TABLOCK,                -- Use table-level locking for the bulk insert
+            ROWTERMINATOR = '\n',   -- Specify the row terminator
+            FORMAT = 'CSV'          -- Specify the format as CSV
+        );
+        SET @endTime = GETDATE();  -- Record the end time of the operation
+        PRINT '>> LOAD DURATION: ' + CAST(DATEDIFF(SECOND, @startTime, @endTime) AS NVARCHAR(10)) + ' seconds';
+        PRINT '>>Bulk insert into bronze.crm_prd_info completed.';
+        PRINT '================================================================';
 
-    -- Truncate the table bronze.crm_sales_details
-    IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
-    BEGIN
-        PRINT '>>Truncating table bronze.crm_sales_details';
-        TRUNCATE TABLE bronze.crm_sales_details;
-    END
-    ELSE
-        PRINT '>>Table bronze.crm_sales_details does not exist.';
+        -- Truncate the table bronze.crm_sales_details
+        IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+        BEGIN
+            PRINT '>>Truncating table bronze.crm_sales_details';
+            TRUNCATE TABLE bronze.crm_sales_details;
+        END
+        ELSE
+            PRINT '>>Table bronze.crm_sales_details does not exist.';
 
-    -- Bulk Insert CRM Sales Details
-    PRINT '>>Bulk inserting data into bronze.crm_sales_details ';
-    Bulk Insert bronze.crm_sales_details
-    FROM 'D:\Github\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-    WITH (
-        FIRSTROW = 2,           -- Skip the header row
-        FIELDTERMINATOR = ',',  -- Specify the field terminator
-        TABLOCK,                -- Use table-level locking for the bulk insert
-        ROWTERMINATOR = '\n',   -- Specify the row terminator
-        FORMAT = 'CSV'          -- Specify the format as CSV
-    );
-    PRINT '>>Bulk insert into bronze.crm_sales_details completed.';
-    PRINT '================================================================';
-    PRINT '>>All CRM data has been successfully loaded into the bronze layer.';
-    PRINT '================================================================';
+        SET @startTime = GETDATE();  -- Record the start time of the operation
+        -- Bulk Insert CRM Sales Details
+        PRINT '>>Bulk inserting data into bronze.crm_sales_details ';
+        Bulk Insert bronze.crm_sales_details
+        FROM 'D:\Github\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+        WITH (
+            FIRSTROW = 2,           -- Skip the header row
+            FIELDTERMINATOR = ',',  -- Specify the field terminator
+            TABLOCK,                -- Use table-level locking for the bulk insert
+            ROWTERMINATOR = '\n',   -- Specify the row terminator
+            FORMAT = 'CSV'          -- Specify the format as CSV
+        );
+        SET @endTime = GETDATE();  -- Record the end time of the operation
+        PRINT '>> LOAD DURATION: ' + CAST(DATEDIFF(SECOND, @startTime, @endTime) AS NVARCHAR(10)) + ' seconds';
+        PRINT '>>Bulk insert into bronze.crm_sales_details completed.';
+        PRINT '================================================================';
+        PRINT '>>All CRM data has been successfully loaded into the bronze layer.';
+        PRINT '================================================================';
+    END TRY
+    BEGIN CATCH
+        -- Handle any errors that occur during the bulk insert operation
+        PRINT '================================================================';
+        PRINT 'An error occurred during the bulk insert operation.';
+        PRINT 'Error Message: ' + ERROR_MESSAGE();
+        PRINT 'Error Number: ' + CAST(ERROR_NUMBER() AS NVARCHAR(10));
+        PRINT 'Error Severity: ' + CAST(ERROR_SEVERITY() AS NVARCHAR(10));
+        PRINT 'Error State: ' + CAST(ERROR_STATE() AS NVARCHAR(10));
+        PRINT 'Error Line: ' + CAST(ERROR_LINE() AS NVARCHAR(10));
+        PRINT '================================================================';
+        -- Optionally, you can log the error to a table or take other actions
+    END CATCH
 END
 -- Execute the procedure to load data into the bronze layer CRM
 GO
--- scripts/erp_bulk_insert.sql
+++ scripts/erp_bulk_insert.sql
@@ -9,78 +9,102 @@ Create or alter procedure bronze.load_erp_bronze_data
 Create or alter procedure bronze.load_erp_bronze_data
 AS
 BEGIN
+    DECLARE @startTime DATETIME, @endTime DATETIME;
+    BEGIN TRY
     -- Truncate the tables if they already exist to ensure fresh data insertion
-    IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
-    BEGIN
+        IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+        BEGIN
+            PRINT '================================================================';
+            PRINT 'Truncating table bronze.erp_cust_az12';
+            TRUNCATE TABLE bronze.erp_cust_az12;
+        END
+        ELSE
+            PRINT 'Table bronze.erp_cust_az12 does not exist.';
+
+        SET @startTime = GETDATE();  -- Record the start time of the operation
+        -- Bulk Insert ERP Customer AZ12 
+        PRINT 'Bulk inserting data into bronze.erp_cust_az12 ';   
+        Bulk Insert bronze.erp_cust_az12
+        -- Save the .csv dataset files in the specified path or modify the path accordingly
+        FROM 'D:\Github\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+        WITH (
+            FIRSTROW = 2,           -- Skip the header row
+            FIELDTERMINATOR = ',',  -- Specify the field terminator
+            TABLOCK,                -- Use table-level locking for the bulk insert
+            ROWTERMINATOR = '\n',   -- Specify the row terminator
+            FORMAT = 'CSV'          -- Specify the format as CSV
+        );
+        SET @endTime = GETDATE();  -- Record the end time of the operation
+        PRINT '>> LOAD DURATION: ' + CAST(DATEDIFF(SECOND, @startTime, @endTime) AS NVARCHAR(10)) + ' seconds';
+        PRINT 'Bulk insert into bronze.erp_cust_az12 completed.';
         PRINT '================================================================';
-        PRINT 'Truncating table bronze.erp_cust_az12';
-        TRUNCATE TABLE bronze.erp_cust_az12;
-    END
-    ELSE
-        PRINT 'Table bronze.erp_cust_az12 does not exist.';
-    
-    -- Bulk Insert ERP Customer AZ12 
-    PRINT 'Bulk inserting data into bronze.erp_cust_az12 ';   
-    Bulk Insert bronze.erp_cust_az12
-    -- Save the .csv dataset files in the specified path or modify the path accordingly
-    FROM 'D:\Github\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
-    WITH (
-        FIRSTROW = 2,           -- Skip the header row
-        FIELDTERMINATOR = ',',  -- Specify the field terminator
-        TABLOCK,                -- Use table-level locking for the bulk insert
-        ROWTERMINATOR = '\n',   -- Specify the row terminator
-        FORMAT = 'CSV'          -- Specify the format as CSV
-    );
-    PRINT 'Bulk insert into bronze.erp_cust_az12 completed.';
-    PRINT '================================================================';
-    
-    -- Truncate the table bronze.erp_loc_a101
-    IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
-    BEGIN
-        PRINT 'Truncating table bronze.erp_loc_a101';
-        TRUNCATE TABLE bronze.erp_loc_a101;
-    END
-    ELSE
-        PRINT 'Table bronze.erp_loc_a101 does not exist.';
 
-    -- Bulk Insert ERP Location A101  
-    PRINT 'Bulk inserting data into bronze.erp_loc_a101 ';  
-    Bulk Insert bronze.erp_loc_a101
-    FROM 'D:\Github\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
-    WITH (
-        FIRSTROW = 2,           -- Skip the header row
-        FIELDTERMINATOR = ',',  -- Specify the field terminator
-        TABLOCK,                -- Use table-level locking for the bulk insert
-        ROWTERMINATOR = '\n',   -- Specify the row terminator
-        FORMAT = 'CSV'          -- Specify the format as CSV
-    );
-    PRINT 'Bulk insert into bronze.erp_loc_a101 completed.';
-    PRINT '================================================================';
+        -- Truncate the table bronze.erp_loc_a101
+        IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+        BEGIN
+            PRINT 'Truncating table bronze.erp_loc_a101';
+            TRUNCATE TABLE bronze.erp_loc_a101;
+        END
+        ELSE
+            PRINT 'Table bronze.erp_loc_a101 does not exist.';
 
-    -- Truncate the table bronze.erp_px_cat_g1v2
-    IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
-    BEGIN
-        PRINT 'Truncating table bronze.erp_px_cat_g1v2';
-        TRUNCATE TABLE bronze.erp_px_cat_g1v2; 
-    END
-    ELSE
-        PRINT 'Table bronze.erp_px_cat_g1v2 does not exist.';
-    
-    -- Bulk Insert ERP Product Category G1V2
-    PRINT 'Bulk inserting data into bronze.erp_px_cat_g1v2 ';
-    Bulk Insert bronze.erp_px_cat_g1v2  
-    FROM 'D:\Github\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
-    WITH (
-        FIRSTROW = 2,           -- Skip the header row
-        FIELDTERMINATOR = ',',  -- Specify the field terminator
-        TABLOCK,                -- Use table-level locking for the bulk insert
-        ROWTERMINATOR = '\n',   -- Specify the row terminator
-        FORMAT = 'CSV'          -- Specify the format as CSV
-    );  
-    PRINT 'Bulk insert into bronze.erp_px_cat_g1v2 completed.';
-    PRINT '================================================================';
-    PRINT '>>All ERP data has been successfully loaded into the bronze layer.';
-    PRINT '================================================================';
+        SET @startTime = GETDATE();  -- Record the start time of the operation
+        -- Bulk Insert ERP Location A101  
+        PRINT 'Bulk inserting data into bronze.erp_loc_a101 ';  
+        Bulk Insert bronze.erp_loc_a101
+        FROM 'D:\Github\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+        WITH (
+            FIRSTROW = 2,           -- Skip the header row
+            FIELDTERMINATOR = ',',  -- Specify the field terminator
+            TABLOCK,                -- Use table-level locking for the bulk insert
+            ROWTERMINATOR = '\n',   -- Specify the row terminator
+            FORMAT = 'CSV'          -- Specify the format as CSV
+        );
+        SET @endTime = GETDATE();  -- Record the end time of the operation
+        PRINT '>> LOAD DURATION: ' + CAST(DATEDIFF(SECOND, @startTime, @endTime) AS NVARCHAR(10)) + ' seconds';
+        PRINT 'Bulk insert into bronze.erp_loc_a101 completed.';
+        PRINT '================================================================';
+
+        -- Truncate the table bronze.erp_px_cat_g1v2
+        IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+        BEGIN
+            PRINT 'Truncating table bronze.erp_px_cat_g1v2';
+            TRUNCATE TABLE bronze.erp_px_cat_g1v2; 
+        END
+        ELSE
+            PRINT 'Table bronze.erp_px_cat_g1v2 does not exist.';
+
+        SET @startTime = GETDATE();  -- Record the start time of the operation
+        -- Bulk Insert ERP Product Category G1V2
+        PRINT 'Bulk inserting data into bronze.erp_px_cat_g1v2 ';
+        Bulk Insert bronze.erp_px_cat_g1v2  
+        FROM 'D:\Github\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+        WITH (
+            FIRSTROW = 2,           -- Skip the header row
+            FIELDTERMINATOR = ',',  -- Specify the field terminator
+            TABLOCK,                -- Use table-level locking for the bulk insert
+            ROWTERMINATOR = '\n',   -- Specify the row terminator
+            FORMAT = 'CSV'          -- Specify the format as CSV
+        );  
+        SET @endTime = GETDATE();  -- Record the end time of the operation
+        PRINT '>> LOAD DURATION: ' + CAST(DATEDIFF(SECOND, @startTime, @endTime) AS NVARCHAR(10)) + ' seconds';
+        PRINT 'Bulk insert into bronze.erp_px_cat_g1v2 completed.';
+        PRINT '================================================================';
+        PRINT '>>All ERP data has been successfully loaded into the bronze layer.';
+        PRINT '================================================================';
+    END TRY
+    BEGIN CATCH
+        -- Handle any errors that occur during the bulk insert operation
+        PRINT '================================================================';
+        PRINT 'An error occurred during the bulk insert operation.';
+        PRINT 'Error Message: ' + ERROR_MESSAGE();
+        PRINT 'Error Number: ' + CAST(ERROR_NUMBER() AS NVARCHAR(10));
+        PRINT 'Error Severity: ' + CAST(ERROR_SEVERITY() AS NVARCHAR(10));
+        PRINT 'Error State: ' + CAST(ERROR_STATE() AS NVARCHAR(10));
+        PRINT 'Error Line: ' + CAST(ERROR_LINE() AS NVARCHAR(10));
+        PRINT '================================================================';
+        -- Optionally, you can log the error to a table or take other actions
+    END CATCH   
 END
 -- execute the procedure to load data into the bronze layer ERP
 GO
