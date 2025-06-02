517c4049b4b6f24443d3bd469940a82ccc5f2100
Amans-2201 <160013204+Amans-2201@users.noreply.github.com>
2025-06-02 20:30:17 +0530

    updated datatype for crm ddl script

--- scripts/Silver/crm_ddl.sql
+++ scripts/Silver/crm_ddl.sql
@@ -3,6 +3,7 @@ GO
 -- Check If the table silver.crm_cust_info already exists and drop it if it does
 IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
     DROP TABLE silver.crm_cust_info;
+GO
 
 -- Create table in silver layer for CRM customer information    
 -- This table contains customer details including ID, name,
@@ -17,14 +18,17 @@ CREATE TABLE silver.crm_cust_info (
     dwh_create_date DATETIME2 DEFAULT GETDATE() -- Data Warehouse Creation Date
 );
 GO
+
 -- Check If the table silver.crm_prd_info already exists and drop it if it does
 IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
     DROP TABLE silver.crm_prd_info;
+GO
 
 -- Create table product information in silver layer for CRM
 -- This table contains details about products including their cost, line, and availability dates
 CREATE TABLE silver.crm_prd_info (
     prd_id INT,                      -- Product ID
+    cat_id NVARCHAR(50),             -- Category ID
     prd_key NVARCHAR(50),            -- Product Key
     prd_name NVARCHAR(50),           -- Product Name
     prd_cost INT,          -- Product Cost
@@ -33,6 +37,8 @@ CREATE TABLE silver.crm_prd_info (
     prd_end_date DATE,              -- Product End Date
     dwh_create_date DATETIME2 DEFAULT GETDATE() -- Data Warehouse Creation Date
 );
+GO
+
 -- Check If the table silver.crm_sales_details already exists and drop it if it does
 IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
     DROP TABLE silver.crm_sales_details;        
@@ -44,9 +50,9 @@ CREATE TABLE silver.crm_sales_details (
     sls_ord_num NVARCHAR(50),                 -- Sales Order Number
     sls_prd_key NVARCHAR(50),        -- Product Key
     sls_cust_id INT,                 -- Customer ID
-    sls_order_date INT,               -- Order Date
-    sls_ship_date INT,                -- Shipping Date
-    sls_due_date INT,                 -- Due Date
+    sls_order_date Date,               -- Order Date
+    sls_ship_date Date,                -- Shipping Date
+    sls_due_date Date,                 -- Due Date
     sls_sales INT,        -- Sales Amount
     sls_quantity INT,                 -- Quantity Sold
     sls_price INT,          -- Price per Unit
