e62529aa2654517fc2ca0aebfede70702709cd6a
Amans-2201 <160013204+Amans-2201@users.noreply.github.com>
2025-06-04 14:36:34 +0530

    added comments

--- scripts/Gold/integration.sql
+++ scripts/Gold/integration.sql
@@ -1,6 +1,18 @@
--- =====================================================================
--- Build Gold layer for Customer, Product  as Dimension and Sales as Fact
--- =====================================================================
+/*
+==============================================================================
+DDL Script: Create Gold Views
+==============================================================================
+Script Purpose:
+    This script creates views for the Gold layer in the data warehouse.
+    The Gold layer represents the final dimension and fact tables (Star Schema)
+
+    Each view performs transformations and combines data from the Silver layer
+    to produce a clean, enriched, and business-ready dataset.
+
+Usage:
+    - These views can be queried directly for analytics and reporting.
+==============================================================================
+*/
 Use DataWarehouse;
 GO
 
